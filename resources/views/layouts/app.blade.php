585617a1f684891dc536e9f128f6563d23f78e1e
ya0201 <you@example.com>
2021-04-12 22:37:48 +0900

    Make it beautiful

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,24 @@
-// resources/views/layouts/app.blade.php
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <title>Laravel Quickstart - Basic</title>
 
-        <!-- CSS And JavaScript -->
+        <!-- Fonts -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+
+        <!-- Styles -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+        <style>
+            body {
+                font-family: 'Lato';
+            }
+            .fa-btn {
+                margin-right: 6px;
+            }
+        </style>
     </head>
 
     <body>
